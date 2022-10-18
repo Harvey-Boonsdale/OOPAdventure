4a08d393bf40b441611eef92fbe12ea16055a9bc
Harvey-Boonsdale <grahampeterbooth@gmail.com>
2022-10-18 16:24:07 +0100

    needs fixing still

--- index.js
+++ index.js
@@ -32,11 +32,11 @@ class Character {
   }
 
   speak() {
-    console.log("My name is " + this.name + ". Fear my " + this.power);
+    return "My name is " + this.name + ". Fear my " + this.power;
   }
 
   attack() {
-    console.log(this.name + " attacks with " + this.inventory);
+    return this.name + " attacks with " + this.inventory;
   }
 }
 
@@ -45,7 +45,7 @@ kitchen.description =
   "It is looking rather worse for wear with a two ring burner, cheap microwave and a cupboard with a broken door.";
 const frontRoom = new Room("Front Room");
 frontRoom.description =
-  "The paint is peeling off the walls and there is clearly a damp problem.";
+  "The paint is peeling off the walls and there is clearly a damp problem. You pick up a box of rat poison from the table";
 const entranceHall = new Room("Entrance Hall");
 entranceHall.description = "There is an odd smell.";
 const diningRoom = new Room("Dining Room");
@@ -54,18 +54,20 @@ diningRoom.description = "You wouldn't want to eat in here.";
 kitchen.linkRoom("south", entranceHall);
 kitchen.linkRoom("east", diningRoom);
 entranceHall.linkRoom("north", kitchen);
-entranceHall.linkRoom("east", frontRoom);
 frontRoom.linkRoom("north", diningRoom);
-frontRoom.linkRoom("west", entranceHall);
 diningRoom.linkRoom("west", kitchen);
 diningRoom.linkRoom("south", frontRoom);
 
 const user1 = new Character("Graham");
-user1.inventory = "hammer";
 user1.power = "knowledge of my rights as a tenant";
 const enemy1 = new Character("Evil Landlord");
 enemy1.inventory = "frying pan";
 enemy1.power = "ability to raise your rent";
+const enemy2 = new Character("Rats");
+enemy2.inventory;
+("Teeth");
+enemy2.power;
+("Rabies");
 
 function displayRoomInfo(room) {
   document.getElementById("textarea").innerHTML = room.describe();
@@ -79,4 +81,22 @@ function startGame() {
 
 startGame();
 
-console.log("Hi World");
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    command = document.getElementById("ui").value;
+    const directions = ["north", "south", "east", "west"];
+    if (directions.includes(command.toLowerCase())) {
+      currentRoom = currentRoom.moveBy(command);
+      displayRoomInfo(currentRoom);
+    } else {
+      document.getElementById("ui").value = "";
+      alert("That is not a valid command, please try again");
+    }
+  }
+});
+
+if (currentRoom.name === "Front Room") {
+  user1.inventory = "Poison";
+}
+
+console.log(user1.inventory);
